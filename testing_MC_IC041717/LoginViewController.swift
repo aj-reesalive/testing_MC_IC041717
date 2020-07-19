9e76e9ca415f94d7a54c3d754c197acb04d2ad37
aj <aj@reesa.live>
2020-07-18 23:09:14 -0700

    google sign in setup part 1 finished

---
+++ testing_MC_IC041717/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  testing_MC_IC041717
+//
+//  Created by AJ on 7/18/20.
+//  Copyright © 2020 AJ. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
