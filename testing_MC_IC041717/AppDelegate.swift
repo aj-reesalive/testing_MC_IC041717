3ad6bf2e366d9643f031757ea04b3cfc9febad43
aj <aj@reesa.live>
2020-07-19 01:13:35 -0700

    googleSignIn completed

--- testing_MC_IC041717/AppDelegate.swift
+++ testing_MC_IC041717/AppDelegate.swift
@@ -11,13 +11,17 @@ import Firebase
 import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        // Use Firebase library to configure APIs
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
         return true
     }
 
@@ -33,6 +37,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance().handle(url)
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+      // ...
+        if let error = error {
+            print(error.localizedDescription)
+        return
+      }
+        print("Google Login Successful")
+
+
+      guard let authentication = user.authentication else { return }
+      let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+      //signin firebase
+        Auth.auth().signIn(with: credential, completion:  { (user, error) in
+            if let err = error {
+                  print(err.localizedDescription)
+              return
+            }
+            print("Successfully logged into Firebase")
+        })
+    }
+
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
     }
 
 
--- testing_MC_IC041717/LoginViewController.swift
+++ testing_MC_IC041717/LoginViewController.swift
@@ -11,22 +11,72 @@ import Firebase
 import GoogleSignIn
 
 class LoginViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    @IBOutlet weak var googleSigninBtn: GIDSignInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance().signIn()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        if Auth.auth().currentUser?.uid != nil {
+            goToHome()
+        }
     }
-    */
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+      // ...
+      if let error = error {
+        print(error)
+        return
+      }
+        print("Signed into google successfully")
 
+
+      guard let authentication = user.authentication else { return }
+      let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        Auth.auth().signIn(with: credential)  { (user, error) in
+            if let error = error {
+                print (error.localizedDescription)
+                return
+            }
+            print("Signed into Firebase successfully")
+        }
+    }
+    
+    func login(){
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion:  { (user, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            self.goToHome()
+        })
+    }
+    
+    func goToHome(){
+        let homePVC = RootPageViewController()
+        self.present(homePVC, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func loginBtn(_ sender: Any) {
+        login()
+    }
+    
+    @IBAction func googleSigninBtn(_ sender: Any) {
+    }
+    
+    
 }
