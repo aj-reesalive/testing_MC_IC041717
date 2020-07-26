2701c5d08c342fd6995b0ef9221be5d9146ea182
aj <aj@reesa.live>
2020-07-25 21:24:53 -0700

    SDWebImage initial pod set up

---
+++ testing_MC_IC041717/EditProViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EditProViewController.swift
+//  testing_MC_IC041717
+//
+//  Created by AJ on 7/25/20.
+//  Copyright © 2020 AJ. All rights reserved.
+//
+
+import UIKit
+
+class EditProViewController: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
